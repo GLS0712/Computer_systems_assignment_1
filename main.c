b616943894db5fe68e6e2f454b4e2fc5befd4e13
GedeGustav <gustavsvare@gmail.com>
2026-09-11 01:10:18 +0200

    chungus erosion virker nu

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 //To compile (win): gcc cbmp.c main.c -o main.exe -std=c99
 //To run (win): main.exe example.bmp example_inv.bmp
+//has to be in your computer terminal not the vs-code one
+
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -9,7 +11,7 @@ void convert_to_grayscale(unsigned char image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNEL
                           unsigned char gray[BMP_WIDTH][BMP_HEIGTH]);
 void apply_threshold(unsigned char gray[BMP_WIDTH][BMP_HEIGTH],
                      unsigned char binary[BMP_WIDTH][BMP_HEIGTH], int threshold);
-void erode_image(unsigned char binary[BMP_WIDTH][BMP_HEIGTH],
+int erode_image(unsigned char binary[BMP_WIDTH][BMP_HEIGTH],
                  unsigned char eroded[BMP_WIDTH][BMP_HEIGTH]);
 
 int main(int argc, char *argv[])
@@ -23,8 +25,11 @@ int main(int argc, char *argv[])
     char *output_path = argv[2];
 
     static unsigned char color_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+
     static unsigned char gray_image[BMP_WIDTH][BMP_HEIGTH];
     static unsigned char binary_image[BMP_WIDTH][BMP_HEIGTH];
+    static unsigned char eroded_image[BMP_WIDTH][BMP_HEIGTH];
+
     static unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
 
     int threshold = 127;
@@ -33,20 +38,44 @@ int main(int argc, char *argv[])
     printf("Loaded '%s' (%d x %d, %d channels)\n",
            input_path, BMP_WIDTH, BMP_HEIGTH, BMP_CHANNELS);
 
-    // flipper farverne til den omvendte altså hvid til sort og sort til hvid
-    // for (int x = 0; x < BMP_WIDTH; x++)
-    // {
-    //     for (int y = 0; y < BMP_HEIGTH; y++)
-    //     {
-    //         for (int c = 0; c < BMP_CHANNELS; c++)
-    //         {
-    //             color_image[x][y][c] = 255 - color_image[x][y][c];
-    //         }
-    //     }
-    // }
 
     convert_to_grayscale(color_image, gray_image);        // updaterer billedet til gray-scale
     apply_threshold(gray_image, binary_image, threshold); // updaterer billedet til sort-hvid
+    
+    
+    // "current" og "next" er pointere til de to buffere (binary_image og eroded_image),
+    // så vi kan skifte mellem dem uden at kopiere hele billedet hver gang (ping-pong buffering)
+    unsigned char (*current)[BMP_HEIGTH] = binary_image;
+    unsigned char (*next)[BMP_HEIGTH] = eroded_image;
+
+    int changed = 1;
+    int passes = 0;
+    char step_filename[256];
+    while (changed) {
+        // kør en erosion: "current" er input, "next" er output
+        // changed bliver 0 når et helt pass ikke fjerner flere pixels (billedet er stabilt)
+        changed = erode_image(current, next);
+        passes++;
+
+        for (int x = 0; x < BMP_WIDTH; x++) {
+            for (int y = 0; y < BMP_HEIGTH; y++) {
+                for (int c = 0; c < BMP_CHANNELS; c++) {
+                    output_image[x][y][c] = next[x][y];
+                }
+            }
+        }
+
+        // gemmer et billede af hvert enkelt skridt, så man kan se erosionen udvikle sig
+        snprintf(step_filename, sizeof(step_filename), "results_example/erode_step_%02d.bmp", passes);
+        write_bitmap(output_image, step_filename);
+
+        // byt om på current og next, så næste iteration eroderer videre på det nye billede
+        unsigned char (*tmp)[BMP_HEIGTH] = current;
+        current = next;
+        next = tmp;
+    }
+    printf("Eroded to completion after %d pass(es)\n", passes);
+
 
     for (int x = 0; x < BMP_WIDTH; x++)
     {
@@ -54,13 +83,12 @@ int main(int argc, char *argv[])
         {
             for (int c = 0; c < BMP_CHANNELS; c++)
             {
-                output_image[x][y][c] = binary_image[x][y];
+                output_image[x][y][c] = current[x][y];
             }
         }
     }
 
     write_bitmap(output_image, output_path);
-
     printf("Wrote thresholded image to '%s'\n", output_path);
     return 0;
 }
@@ -72,6 +100,7 @@ void convert_to_grayscale(unsigned char image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNEL
     {
         for (int y = 0; y < BMP_HEIGTH; y++)
         {
+            // gennemsnit af rød, grøn og blå -> ét gråtone-tal pr. pixel
             int sum = image[x][y][0] + image[x][y][1] + image[x][y][2];
             gray[x][y] = (unsigned char)(sum / 3);
         }
@@ -85,6 +114,7 @@ void apply_threshold(unsigned char gray[BMP_WIDTH][BMP_HEIGTH],
     {
         for (int y = 0; y < BMP_HEIGTH; y++)
         {
+            // under/lig threshold bliver sort (0), resten bliver hvid (255)
             unsigned char value = (gray[x][y] <= threshold) ? 0 : 255;
             for (int c = 0; c < BMP_CHANNELS; c++)
             {
@@ -94,76 +124,39 @@ void apply_threshold(unsigned char gray[BMP_WIDTH][BMP_HEIGTH],
     }
 }
 
-void erode_image(unsigned char binary[BMP_WIDTH][BMP_HEIGTH],
+int erode_image(unsigned char binary[BMP_WIDTH][BMP_HEIGTH],
                  unsigned char eroded[BMP_WIDTH][BMP_HEIGTH])
 {
-    for (int x = 0; x < BMP_WIDTH; x++)
-    {
-        for (int y = 0; y < BMP_HEIGTH; y++)
-        {
-            if (x == 0 && y == 0)
-            {
-                if (binary[x + 1][y] == 0 || binary[x][y + 1] == 0)
-                {
-                    eroded[x][y] = 0;
-                }
+    int changed = 0; // sættes til 1 hvis mindst én pixel bliver eroderet væk i dette pass
+
+    for (int x = 0; x < BMP_WIDTH; x++) {
+        for (int y = 0; y < BMP_HEIGTH; y++) {
+
+            // en pixel der allerede er sort forbliver sort
+            if (binary[x][y] == 0) {
+                eroded[x][y] = 0;
+                continue;
             }
-            else if (x == 255 && y == 255)
-            {
-                if (binary[x - 1][y] == 0 || binary[x][y - 1] == 0)
-                {
-                    eroded[x][y] = 0;
-                }
-            }
-            else if (x == 255 && y == 0)
-            {
-                if (binary[x - 1][y] == 0 || binary[x][y + 1] == 0)
-                {
-                    eroded[x][y] = 0;
-                }
-            }
-            else if (x == 0 && y == 255)
-            {
-                if (binary[x + 1][y] == 0 || binary[x][y - 1] == 0)
-                {
-                    eroded[x][y] = 0;
-                }
-            }
-            else if (x == 0)
-            {
-                if (binary[x + 1][y] == 0 || binary[x][y + 1] == 0 || binary[x][y - 1] == 0)
-                {
-                    eroded[x][y] = 0;
-                }
-            }
-            else if (y == 0)
-            {
-                if (binary[x + 1][y] == 0 || binary[x - 1][y] == 0 || binary[x][y + 1] == 0)
-                {
-                    eroded[x][y] = 0;
-                }
-            }
-            else if (x == 255)
-            {
-                if (binary[x - 1][y] == 0 || binary[x][y + 1] == 0 || binary[x][y - 1] == 0)
-                {
-                    eroded[x][y] = 0;
-                }
-            }
-            else if (y == 255)
-            {
-                if (binary[x - 1][y] == 0 || binary[x + 1][y] == 0 || binary[x][y + 1] == 0 || binary[x][y - 1] == 0)
-                {
-                    eroded[x][y] = 0;
-                }
-            }
-            else
-            {
-                if (binary[x - 1][y] == 0 || binary[x + 1][y] == 0 || binary[x][y + 1] == 0 || binary[x][y - 1] == 0)
-                {
-                    eroded[x][y] = 0;
-                }
+
+            // en hvid pixel overlever kun hvis den IKKE ligger på billedets kant,
+            // og alle 4 naboer (op/ned/venstre/højre) også er hvide
+            int survives =
+                (x > 0) && (x < BMP_WIDTH - 1) && //tjekker at vi er inden for billedet på x-aksen
+                (y > 0) && (y < BMP_HEIGTH - 1) && //tjekker at vi er inden for billedet på y-aksen
+                binary[x - 1][y] == 255 &&
+                binary[x + 1][y] == 255 &&
+                binary[x][y - 1] == 255 &&
+                binary[x][y + 1] == 255;
+
+            if (survives) {
+                eroded[x][y] = 255;
+            } else {
+                // pixel lå på kanten af en hvid region og bliver "spist" væk
+                eroded[x][y] = 0;
+                changed = 1;
             }
         }
     }
+
+    return changed;
 }
